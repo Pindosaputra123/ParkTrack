8aa99a5139c42f59aac89c1904dcc55f37c5ec53
Pindosaputra123 <pindoajja864@gmail.com>
2024-12-23 09:01:07 +0700

    Menambahkan Folder Dan File Komponen HTML Yang Digunakan Pada Projek

---
+++ components/head-tags.php
@@ -0,0 +1,38 @@
+<!-- tailwind -->
+<script src="https://cdn.tailwindcss.com"></script>
+
+<!-- chart/grafik -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<!-- font awesome icon -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- tooltip -->
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/tippy.js@6"></script>
+
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+<!-- matikan touch highlight untuk mobile-->
+<style>
+  * {
+    -webkit-tap-highlight-color: transparent;
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
+
+<!-- tailwind config -->
+<script>
+  tailwind.config = {
+    darkMode: "class",
+  }
+</script>
+
+
+<!--meta tags-->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
